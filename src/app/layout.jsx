675849ce91de922d1dca5aa8374ae97f4a212292
Nama Anda <muhammadfaizfaqih123@gmail.com>
2026-06-29 08:39:39 +0700

    favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,14 @@
 import "./globals.css";
-import Script from "next/script";
 
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" style={{ overflowX: 'hidden', maxWidth: '100vw' }}>
+      <body style={{ overflowX: 'hidden', maxWidth: '100vw', margin: 0, padding: 0 }}>
+        <div id="__next" style={{ overflowX: 'hidden', width: '100%' }}>
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import styles from './page.module.css';
 import Image from 'next/image';
+import "./globals.css";
 
 export default function Home() {
   return (
